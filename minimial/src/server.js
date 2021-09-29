f312d1a9ba46e7fa0ad4338642f34513d8176ae3
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-09-29 11:03:00 +0500

    Imported all needed things and created a server connection self invoked function

---
+++ minimial/src/server.js
@@ -0,0 +1,19 @@
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { ApolloServer } = require('apollo-server');
+
+const connectToMongoDB = require('./db/connect');
+
+require('colors');
+
+const PORT = process.env.PORT || 5000;
+
+connectToMongoDB();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+(async () => {
+    server.listen(PORT, () => {
+        console.log(`[server]: Server is ready at http://localhost:${PORT}`.yellow.bold.underline);
+    });
+})();
